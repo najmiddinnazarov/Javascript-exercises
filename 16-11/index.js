8221c1e8a304ca78f473c5a233fdc0456ce2bd90
najmiddinnazarov <najmiddinnazarov0405@gmail.com>
2022-11-18 04:58:42 +0500

    Multiplication table, for loop

---
+++ 16-11/index.js
@@ -0,0 +1,7 @@
+for (let i = 2; i <= 9; i++) {
+  console.log(`Ko'paytirish jadvali ${i}`);
+  for (let j = 1; j <= 10; j++) {
+    let result = i * j;
+    console.log(`${i} * ${j} = ${result}`);
+  }
+}
