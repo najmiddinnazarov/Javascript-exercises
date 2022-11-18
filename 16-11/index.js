4ffa276153f1237b3987a2c4840e7db3619445fa
najmiddinnazarov <najmiddinnazarov0405@gmail.com>
2022-11-18 05:18:07 +0500

    Multiplication table, for loop ,switch statement

--- 16-11/index.js
+++ 16-11/index.js
@@ -1,7 +1,40 @@
-for (let i = 2; i <= 9; i++) {
-  console.log(`Ko'paytirish jadvali ${i}`);
-  for (let j = 1; j <= 10; j++) {
-    let result = i * j;
-    console.log(`${i} * ${j} = ${result}`);
-  }
-}
+// for (let i = 2; i <= 9; i++) {
+//   console.log(`Ko'paytirish jadvali ${i}`);
+//   for (let j = 1; j <= 10; j++) {
+//     let result = i * j;
+//     console.log(`${i} * ${j} = ${result}`);
+//   }
+// }
+
+// ============= =================== //
+
+// let browser = "Edge";
+// switch (browser) {
+//   case "Edge":
+//     console.log("You've got the Edge!");
+//     break;
+//   case "Chrome":
+//   case "Firefox":
+//   case "Safari":
+//   case "Opera":
+//     console.log("Okay we support these browsers too");
+//     break;
+//   default:
+//     console.log("We hope that this page looks ok!");
+// }
+// browser degan o'zgaruvchimizga biron bir browser nomini kiritganimizda. Tepadan pastga qarab case larni yurib chiqadi va qaysi case ga bizni browser imiz to'g'ri kelsa o'sha case dagi code ni console ga chiqaradi.
+
+// ============= =================== //
+
+// let a = 3;
+// if (a == 0) {
+//   console.log(0);
+// }
+// if (a == 1) {
+//   console.log(1);
+// }
+// if (a == 2 || a == 3) {
+//   console.log("2,3");
+// }
+
+// a degan o'zgaruvchimizga biron bir raqam kiritganimizda. Tepadan pastga qarab if larni yurib chiqadi va qaysi if ga bizni number imiz to'g'ri kelsa o'sha if dagi code ni console ga chiqaradi. Va qolgan if larni ham baribir o'qib chiqadi. Chunki alohida 3 ta if blocki ochilgan.
