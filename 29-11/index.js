00b96c97957b1e21b14af86467f39b95d0e80f06
najmiddinnazarov <najmiddinnazarov0405@gmail.com>
2022-11-29 23:32:37 +0500

    Add number theme exercises

---
+++ 29-11/index.js
@@ -0,0 +1,18 @@
+// const number = (num) => {
+//   const obj = {};
+//   let result = num.toString(2);
+//   console.log(result);
+//   for (const i of result) {
+//     !obj[i] ? (obj[i] = 1) : obj[i]++;
+//   }
+//   return obj;
+// };
+// console.log(number(17));
+
+// const result = Math.max(1, 3, 4, 5, 6) - Math.min(1, 3, 4, 5, 6);
+// console.log(result);
+
+// const randomInteger = (min, max) => {
+//   return Math.floor(Math.random() * (max - min) + min);
+// };
+// console.log(randomInteger(1, 50));
